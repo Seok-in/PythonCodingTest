9ee61a850c4d7f1656a2f2106c7af56f794fdd7e
Seok-in <hrpt8@naver.com>
2021-08-28 23:17:05 +0900

    [Update] python coding Test Dynamic Programming

--- Solution/DynamicProgramming/DynamicProgramming04.py
+++ Solution/DynamicProgramming/DynamicProgramming04.py
@@ -1,19 +1,17 @@
-n, m = map(int, input().split())
-
-array =[]
+n, m = map(int, (input().split()))
+array = []
 for i in range(n):
     array.append(int(input()))
+array.sort()
+d =[10001] * (m+1)
+d[0] = 0
 
-d = [1000000] * 10001
+for i in range(n):
+    for j in range(array[i], m+1):
+        if d[j - array[i]] != 10001:
+            d[j] = min(d[j], d[j-array[i]]+1)
 
-
-for i in range(m+1):
-    for k in array:
-        d[k] = 1
-        if i >= k:
-            d[i] = min(d[i], d[i-k]+1)
-
-if d[m] == 1000000:
+if d[m] == 10001:
     print(-1)
 else:
     print(d[m])
